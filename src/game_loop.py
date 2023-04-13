af146def8b437c19ff18bb80ee3bd61128769394
Elias Junno <junnoelias@gmail.com>
2023-04-13 10:35:53 +0300

    add mouse input to game loop

--- src/game_loop.py
+++ src/game_loop.py
@@ -1,6 +1,18 @@
 import pygame
 
 FPS = 60
+MOUSE_POSITION = pygame.mouse.get_pos()
+
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
 class GameLoop:
     def __init__(self, map, clock, renderer, event_queue):
         self._map = map
@@ -13,6 +25,8 @@ class GameLoop:
             if self._handle_events() == False:
                 break
             
+            MOUSE_POSITION = pygame.mouse.get_pos()
+            
             # Time/ticks elapsed since game start.
             current_time = self._clock.get_ticks()
             
@@ -23,8 +37,15 @@ class GameLoop:
     
     def _handle_events(self):
         for event in pygame.event.get():
+            # Check if a escape is pressed
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    return False
+            
             if event.type == pygame.QUIT:
                 return False
+            
+
     
     def _render(self):
         self._renderer.render()
