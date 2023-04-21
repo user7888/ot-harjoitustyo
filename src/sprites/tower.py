cfceeee32a3b734ae68057c9f54e58a2cad74e9a
Elias Junno <junnoelias@gmail.com>
2023-04-21 18:10:44 +0300

    fix tower position when placing towers

--- src/sprites/tower.py
+++ src/sprites/tower.py
@@ -13,6 +13,9 @@ class Tower(pygame.sprite.Sprite):
         self.image = pygame.image.load(
             os.path.join(dirname, "..", "assets", "tower.png")
         )
+        # Scale the image
+        self.image_scaled = pygame.transform.scale(self.image, (70, 70))
+        self.image = self.image_scaled
         # Define the size for the object. Use
         # the dimensions of the monster image.
         self.rect = self.image.get_rect()
